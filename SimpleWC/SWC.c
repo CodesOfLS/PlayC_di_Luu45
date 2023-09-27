b42a90adab7aa391e4d4c3bce76cfab17629cb14
Luu45 <contatopyluu@gmail.com>
2023-09-27 18:56:21 -0300

    Criação do SimpleWC e adição do código base

---
+++ SimpleWC/SWC.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+/*
+    Simple WC
+
+    SWC <op> <default: -w>
+        -l   Contar linhas
+        -c   Contar caracteres
+        -w   Contar palavras
+*/
+int main(int argc, char *argv[])
+{
+    int c, cw, cl;
+    c = cw = cl = 0;
+    
+    printf("%d\n", argc - 1);
+    
+    /* ... */
+
+    return 0;
+}
